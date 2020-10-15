1864bdfa6627bced53ab9af681124b6df7cf1446
unknown <es-amira.khalid2018@alexu.edu.eg>
2020-10-15 15:54:06 +0200

    todo with react

---
+++ toDoReact/my-app/src/components/App.jsx
@@ -0,0 +1,118 @@
+import React, { Fragment } from 'react';
+import '../assets/scss/app.scss'
+import Nav from '../layouts/Nav';
+import TaskItem from './tasks/TaskItem';
+
+export default class App extends React.Component {
+
+    state = {
+
+        tasks: [
+            {
+                id: 1,
+                title: 'task 1',
+                desc: 'our first task',
+                isDone: false
+            },
+            {
+                id: 2,
+                title: 'task 2',
+                desc: 'our 2nd task',
+                isDone: true
+            }
+
+
+        ],
+          newTask:{
+            title: 'task 2',
+            desc: 'our 2nd task',
+            isDone: false
+
+          },
+        isAddNewTask:false
+
+
+    }
+   updateIsAddNewTask=()=>{
+       this.setState({
+        isAddNewTask:! this.state.isAddNewTask
+       });
+   }
+
+   onChangeHandler=e=>{
+       const newTask = this.state.newTask;
+       newTask[e.target.id]=e.target.value;
+   this.setState({
+    newTask:newTask
+   })
+   }
+
+   onSubmitHandler=e=>{
+       e.preventDefault();
+
+       const tasks=this.state.tasks;
+       tasks.push(this.state.newTask)
+       this.setState({
+           tasks:tasks
+       })
+   }
+
+    toggleDone=()=>{
+        
+    this.setState({
+        isDone:! this.state.tasks.isDone
+       });
+        
+}   
+   
+   
+
+
+
+    render() {
+
+        return (
+
+            <Fragment>
+
+                <Nav />
+                <div className="container">
+                    <h1>Tasks</h1>
+                    <div className="row justify-content-center">
+                        <div className="col-md-8">
+                        <button className={`btn btn-success ${this.state.isAddNewTask?'d-none':''} `}onClick={this.updateIsAddNewTask} >Add Task</button>
+                        <form onSubmit={this.onSubmitHandler} className={`w-100  ${this.state.isAddNewTask?'':'d-none'}`}>
+                            <div className="form-group">
+                                <label htmlFor="title">Title</label>
+                                <input type="text" id="title" className='form-control' onChange={this.onChangeHandler} />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="desc">Description</label>
+                                <textarea  id="desc" cols="30" rows="10" className='form-control'
+                                 onChange={this.onChangeHandler}/>
+                            </div>
+                            <div className='text-center'>
+                                <button  className='btn btn-success m-3' >Save</button>
+                                <button type='button' className='btn btn-warning m-3' onClick={this.updateIsAddNewTask} >Close</button>
+                            </div>
+
+                        </form>
+                        </div>
+                            {this.state.tasks.map(function (task) {
+                                return (
+                                   <TaskItem key={task.id} task={task} toggleDone={this.toggleDone}/> 
+
+                                )
+                            })}
+
+                    
+                    </div>
+
+                </div>
+
+            </Fragment>
+        )
+    }
+}
+
---
+++ toDoReact/my-app/src/components/tasks/TaskItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+  
+
+/*const toggleDone=(task)=>{
+    this.setState({
+        isDone:! task.isDone
+       });
+} */  
+
+
+
+const TaskItem = ( {task,toggleDone}) => {
+    return (
+        <div className="col-md-9">
+
+            <div className="card mt-3">
+
+                <div className="card-body">
+                    <h5 className="card-title">{task.title}</h5>
+                    <p className="card-text">{task.desc}</p>
+                    <button className={`btn btn-${task.isDone ? 'danger' : 'primary'} `} onClick={ toggleDone} >
+                        {task.isDone ? 'Re-open' : 'Done'} </button>
+                        <button className='btn btn-secondary m-3 px-4' >Edit</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default TaskItem;
